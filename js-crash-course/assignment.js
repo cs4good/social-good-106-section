bb92cee7fe01d7bcceddd94666652dae642ff23b
Ashi Agrawal <ashi.agrawal.2015@gmail.com>
2018-04-05 22:05:29 -0700

    Outline for new introductory assignment.

---
+++ js-crash-course/assignment.js
@@ -0,0 +1,79 @@
+/*
+ * Encrypts the given string using the Caesar cipher and the given length.
+ * ----
+ * Returns:
+ * string - the encrypted version of the given string using the given length
+ * Params:
+ * original - string you would like to encrypt
+ * length - given transposition length
+ *
+ */
+function encryptCaesar(original, length) {
+
+}
+
+/*
+ * Determines whether the first string is the encrypted version of the second with a given encryption length.
+ * ----
+ * Returns:
+ * boolean - if encrypted is a valid encryption of guess with given length
+ * Params:
+ * encrypted - given encryption
+ * length - given transposition length
+ * guess - your guess for the unencrypted string
+ *
+ */
+function checkCaesar(encrypted, length, guess) {
+
+}
+
+/*
+ * Determines whether the first string is the encrypted version of the second.
+ * ----
+ * Returns:
+ * boolean - if encrypted is a valid encryption of guess
+ * Params:
+ * encrypted - given encryption
+ * guess - your guess for the unencrypted string
+ *
+ */
+function checkCaesar(encrypted, guess) {
+
+}
+
+/*
+ * Determines whether the first string is the encrypted version of the second.
+ * ----
+ * Returns:
+ * char - shifted letter
+ * Params:
+ * original - letter you'd like to shift
+ * length - length to transpose letters by
+ *
+ */
+function shiftLetter(original, length) {
+
+}
+
+/*
+ * Returns the numerical analog of a letter.
+ */
+function convertChar(char) {
+
+}
+
+/*
+ * *** EXTENSION ***
+ * ----
+ * Encrypts the given string using the Vigenere cipher and the given keyword.
+ * ----
+ * Returns:
+ * string - the encrypted version of the given string using the given keyword
+ * Params:
+ * original - string you would like to encrypt
+ * length - given keyword
+ *
+ */
+function encryptVigenere(original, keyword) {
+
+}
